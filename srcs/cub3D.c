7b260c949512846e3e0fb12967af0e0c8cf96304
Camy-Devlop <adbaibi.ismail@me.com>
2025-12-16 12:40:15 +0100

    test ok

--- srcs/cub3D.c
+++ srcs/cub3D.c
@@ -6,12 +6,12 @@
 /*   By: isadbaib <isadbaib@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/12 21:01:18 by isadbaib          #+#    #+#             */
-/*   Updated: 2025/12/15 12:25:35 by isadbaib         ###   ########.fr       */
+/*   Updated: 2025/12/16 12:36:56 by isadbaib         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int  printf(const char *s, ...);
-#include "../libs/libft/incs/libft.h"
+#include <libft.h>
 int main()
 {
 	char *s="Adbaibi";
