da1a7273ae78931fd0d7c9b0b643a40f11b8e68e
Camy-Devlop <adbaibi.ismail@me.com>
2025-12-18 14:27:19 +0100

    test de lecture de fichier

--- srcs/check/ft_check_files.c
+++ srcs/check/ft_check_files.c
@@ -2,7 +2,7 @@
 #include <libft.h>
 #include <stdbool.h>
 
-bool ft_file_exist(const char *s)
+bool ft_file_exist(const char *s, t_gnl_buf file)
 {
 	
 }
--- srcs/cub3D.c
+++ srcs/cub3D.c
@@ -6,15 +6,21 @@
 /*   By: isadbaib <isadbaib@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/12 21:01:18 by isadbaib          #+#    #+#             */
-/*   Updated: 2025/12/16 12:36:56 by isadbaib         ###   ########.fr       */
+/*   Updated: 2025/12/18 13:02:57 by isadbaib         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int  printf(const char *s, ...);
 #include <libft.h>
+#include <gnl.h>
 int main()
 {
+	t_gnl_buf *file = calloc(1sizeof(t_gnl_buf));
+
+	
 	char *s="Adbaibi";
 	printf("string = %s, longeur =%zu\n",s,ft_strlen(s));
+	
+
 	return (0);
 }
